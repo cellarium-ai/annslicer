948cb562ee27c7a849c875e20c4a1c7bc55a8001
Stephen Fleming <sjfleming@users.noreply.github.com>
2026-07-28 12:32:00 -0400

    linting

--- src/annslicer/slice.py
+++ src/annslicer/slice.py
@@ -174,7 +174,11 @@ def _shard_store(
             sorted_idx = np.sort(orig_idx)
             restore = np.argsort(np.argsort(orig_idx))
             X = _unwrap(adata.X[sorted_idx, :])[restore]
-            layers = {k: _unwrap(adata.layers[k][sorted_idx, :])[restore] for k in adata.layers if k is not None}
+            layers = {
+                k: _unwrap(adata.layers[k][sorted_idx, :])[restore]
+                for k in adata.layers
+                if k is not None
+            }
             obsm = {k: np.asarray(adata.obsm[k][sorted_idx])[restore] for k in adata.obsm}
             obs = adata.obs.iloc[orig_idx]
             ad.AnnData(
